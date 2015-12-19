efb79163f5e0c453bd3934b26801b741c82a7142
Volker E <Volker-E@users.noreply.github.com>
2015-12-19 11:40:18 -0800

    Update set_keyboard_preferences.sh
    Disable set delay until repeat

--- os/os_x/preferences/set_keyboard_preferences.sh
+++ os/os_x/preferences/set_keyboard_preferences.sh
@@ -10,11 +10,11 @@ set_preferences() {
     execute 'defaults write NSGlobalDomain AppleKeyboardUIMode -int 3' \
         'Enable full keyboard access for all controls'
 
-    execute 'defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false' \
-        'Disable press-and-hold in favor of key repeat'
+    #execute 'defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false' \
+    #    'Disable press-and-hold in favor of key repeat'
 
-    execute 'defaults write NSGlobalDomain "InitialKeyRepeat_Level_Saved" -int 15' \
-        'Set delay until repeat'
+    #execute 'defaults write NSGlobalDomain "InitialKeyRepeat_Level_Saved" -int 15' \
+    #    'Set delay until repeat'
 
     execute 'defaults write NSGlobalDomain KeyRepeat -int 2' \
         'Set the key repeat rate to fast'
